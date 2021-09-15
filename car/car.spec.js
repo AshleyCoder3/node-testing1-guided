caf31e2c0c690612f830c1ac946233fb65da5a3a
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-15 18:44:50 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -9,7 +9,7 @@ test('objectMaker returns an object', () => {
   const whatItReturns = objectMaker()
   const whatWeExpect = {}
   expect(whatItReturns).toEqual(whatWeExpect) // toBe will not work here!
-  expect({ name: 'foo'}).toMatchObject({})
+  expect({ name: 'foo', age: 25 }).toMatchObject({ age: 25 })
 })
 test('with truly the same objects we can use .toBe', () => {
   const o = {}
