4349c34f698822b6d0f6f66ce89ac7f1a7e5d35a
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-15 19:21:01 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -20,14 +20,13 @@ test('with truly the same objects we can use .toBe', () => {
 
 describe('car', () => {
   let prius
-  beforeEach(() => {
-    prius = new Car('')
+  beforeEach(() => { // afterEach 
+    prius = new Car('Toyota', 'Prius')
   })
   test('Car is defined', () => {
     expect(Car).toBeDefined()
   })
   test('Car creates instances of cars', () => {
-    const prius = new Car('Toyota', 'Prius')
     expect(prius.make).toBe('Toyota')
     expect(prius.model).toBe('Prius')
     expect(prius).toHaveProperty('make')
@@ -36,11 +35,9 @@ describe('car', () => {
     expect(prius).toMatchObject({ make: 'Toyota', model: 'Prius' })
   })
   test('new cars have an odometer set at zero', () => {
-    const prius = new Car ('Toyota', 'Prius');
     expect(prius.odometer).toBe(0);
   })
   test('cars have a "drive" method', () => {
-    const prius = new Car ('Toyota', 'Prius');
     expect(prius.drive).toBeDefined()
     expect(prius.drive).toBeInstanceOf(Function)
     expect(typeof prius.drive).toBe("function")
